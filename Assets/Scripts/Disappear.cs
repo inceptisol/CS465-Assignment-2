79441641bbe84ac88ff6412112a0fea37ccd75ca
Valdez <chris444@colostate.edu>
2026-04-06 16:24:42 -0600

    grabbable object working but misaligned

--- Assets/Scripts/Disappear.cs
+++ Assets/Scripts/Disappear.cs
@@ -6,18 +6,30 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Disappear : UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable {
 
     [SerializeField] private float speed = 100.0f;
+    Vector3 initialPosition;
+    bool moved = false;
+    bool initiallyMoved = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
     {
         base.ProcessInteractable(updatePhase);
-
+        if (!initiallyMoved)
+        {
+            initialPosition = transform.position;
+            initiallyMoved = true;
+            Debug.Log("initially moved!");
+        }
         if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
         {
-            if (isSelected)
+            if (isSelected && !moved)
             {
-                Debug.Log("Hey there!");
-                Debug.Log("I'm losing my mind!");
+                Debug.Log(Vector3.Distance(initialPosition, transform.position));
+                if (Vector3.Distance(initialPosition, transform.position) > 1)
+                {
+                    Debug.Log("ITS BEEN MOVED");
+                    moved = true;
+                }
             }
         }
     }
