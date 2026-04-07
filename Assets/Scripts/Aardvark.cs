8f4b876956888e87f306f82f7f650928d5ee2cd3
Valdez <chris444@colostate.edu>
2026-04-06 18:19:16 -0600

    sphere changes to green and slightly moves

---
+++ Assets/Scripts/Aardvark.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Aardvark : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Test2()
+    {
+        Debug.Log("Grabbed!!");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
